c3268fca087021c0aacc04e67dcf1aff43428316
Jesslyn Susanto <jesslynsusanto88@gmail.com>
2026-03-10 17:17:29 +1100

    Initial commit for new Unity project

---
+++ Assets/Scripts/QuitKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitKey : MonoBehaviour
+{
+    public KeyCode quitKey;
+
+	private void Start()
+	{
+        Cursor.visible = false;
+	}
+
+	void Update()
+    {
+        if(Input.GetKeyDown(quitKey))
+        {
+            Application.Quit();
+        }
+    }
+}
